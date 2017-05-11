83b70265fd6a4c224ccb7ad52e920d4f3ea1b79d
edoardesd <edoardesd@gmail.com>
2017-05-11 19:43:44 +0200

    added tpl and lq. BUG: not in real time

--- bluedump.py
+++ bluedump.py
@@ -24,7 +24,7 @@ class BluetoothProcessor:
 		self.bt = subprocess.Popen(['l2ping', mac_address], bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		#self.bt2 = subprocess.Popen(['ping', '84:11:9E:FD:16:B2'], bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		self.logger = sys.stdout #open("/logs/dump.log", "a")
-
+		print "Starting at: ", datetime.utcnow().strftime("%H:%M:%S.%f")[:-3]
 		return self.bt
 
 	def process(self):
@@ -37,44 +37,42 @@ class BluetoothProcessor:
 
 		#recupero la stringa che sta scrivendo airodump
 		line = self.bt.stdout.readline()
-		#line2 = self.bt2.stdout.readline()
 		
 		#setto il tempo attuale
 		now = datetime.utcnow().strftime("%H:%M:%S.%f")[:-3]
 
-		#per guardare output di airodump a schermo
-		#if line:
-			#self.logger.write("L:"+line.encode('ascii', errors='ignore'))
+		#chiamo script per recuperare rssi, lq, tpl
+		log_val = subprocess.check_output("./values_log.sh "+mac_address+" ", shell=True)
+		if "Not" in log_val:
+			print "not connected at ", now
 		
+		else:
+			o = re.split(r'\s{4,}', log_val)
+			rssi = o[0]
+			lq = o[1]
+			tpl = o[2].replace("\n", "")
+
 		if not line:
+			return self.client_list, False
+
+		if "Send failed" in line:
 			return self.client_list, False
 		
 		if mac_address not in line:
+			print "not mac at ", now
 			return self.client_list, True
 
 		v = re.split(r'\s{1,}', line)
 		echo_time = v[-2][:-2]
 
 		if echo_time == ".":
-			print echo_time
+			print "not puntino at ", now
 			return self.client_list, True
-
-		print echo_time
 
 		if not self.client_list.has_key(CLIENT):
 			self.client_list[CLIENT] = {}
-			self.client_list[CLIENT]["ping"] = {now: {"echo_time": echo_time}}
 
-		
-		self.client_list[CLIENT]["ping"][now] = {"echo_time": echo_time} 
-
-
-		log_val = subprocess.check_output("./values_log.sh "+mac_address+" ", shell=True)
-	
-
-		o = re.split(r'\s{4,}', log_val)
-		print o
-		self.client_list[CLIENT]["log"] = {now: {"rssi": o[0]}}
+		self.client_list[CLIENT][now] = {"echo_time": echo_time, "rssi": rssi, "lq": lq, "tpl": tpl}
 
 		return self.client_list, True
 
