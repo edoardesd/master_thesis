c6126ab8cc83b6f917b8a30f46c01271dcefdffb
edoardesd <edoardesd@gmail.com>
2018-04-10 12:24:51 +0200

    add rssi local average

--- blewizi/Script_Bash/rssiloop.sh
+++ blewizi/Script_Bash/rssiloop.sh
@@ -12,6 +12,10 @@ while true; do
 	RSSI_val=${OUT#$remove}
 	sum_rssi=$((sum_rssi+RSSI_val))
 	avg=$((sum_rssi/x))
+	if [ $x -eq 30 ]; then
+		x=0
+		sum_rssi=0
+	fi
 	echo "rssi: $RSSI_val  ----- avg rssi: $avg"
 done
 
